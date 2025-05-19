b93f2699e31545afd66bacead3ee24e20495aa4c
hofelenoci943 <hofelenoci943@gmail.com>
2025-05-19 06:15:46 +0000

    Add 38.php

---
+++ src/38.php
@@ -0,0 +1,12 @@
+<?php
+// Example PHP code to demonstrate basic variable usage and basic script structure
+
+// Variable declaration
+$myVariable = "Hello, World!";
+
+// Variable assignment
+$myVariable = "PHP is a powerful language";
+
+// Output statement
+echo $myVariable;
+?>
